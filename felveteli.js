b97847ba1c01c9c5576180172fdb0cebe29bc5dd
szeric461 <93657723+szeric461@users.noreply.github.com>
2025-03-12 11:22:47 +0100

    1.

---
+++ felveteli.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mysql = require("mysql");
+
+app.use(cors());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    port: 3307,
+    password: "",
+    database: "felveteli"
+})
+
+app.get ("/", (req,res) => {
+    res.send("Működik a szerver");
+})
+
+app.get ("/", (req,res) => {
+    
+})
+
+app.listen(3000, () => {
+ 
+    console.log("A felvételi szervere a 3000-es porton fut.");
+ 
+});
